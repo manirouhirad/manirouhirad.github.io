f97935f816ed26add42f66f1076daa8435b158cf
Mani Rouhi Rad <mani.rr2003@gmail.com>
2022-08-18 20:49:12 -0500

    fixed the email

--- build_site.R
+++ build_site.R
@@ -2,7 +2,7 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("/Users/manirouhirad/Dropbox/Website/manirouhirad.github.io/")
+setwd("/Users/mani.rouhirad/Dropbox/Website/manirouhirad.github.io/")
 # ls
 #render your sweet site.
 rmarkdown::render_site()
